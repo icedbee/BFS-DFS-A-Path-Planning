e8a7e6e85074253a1a4ef4dfccd0f6c46a56cd5e
icedbee <anthony@romansystems.com>
2021-09-21 16:25:51 -0400

    Put pathplanner class inside main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import PathPlanner
 import sys
+
+class PathPlanner:
+    def __init__(self, start, goal, grid):
+        self.start = start
+        self.goal = goal
+        self.grid = grid
+
+    def depth_first_search(self):
+        print(self.start)
+    def breadth_first_search(self):
+        print(self.goal)
+    def a_star_search(self):
+        print(self.start)
+
  
 #total arguments
 n = len(sys.argv) #n = 9
