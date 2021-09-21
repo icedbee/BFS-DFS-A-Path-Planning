9a892f3fb2bf314d594c56de3bc3719a8cbd1fbc
Anthony <anthony@romansystems.com>
2021-09-20 23:44:56 -0400

    Added some better command line argument parsing

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import sys
 n = len(sys.argv) #n = 9
 
 if(n > 9):
-    print("Error: Too many command line arguments")
+    print("Error: Too many command line arguments; there should be 9 arguments.")
 elif(n < 9):
-    print("Error: Not enough command line arguments")
+    print("Error: Not enough command line arguments; there should be 9 arguments.")
 else:
     file_name = sys.argv[0] #main.py
     input_string = sys.argv[1] #--input
@@ -18,16 +18,16 @@ else:
     search_string = sys.argv[7] #--search
     search_type = sys.argv[8] #A*
 
-print(start_node[0])
+    if(input_string != "--input"):
+        print("Error: second argument should be --input\n")
+    if(start_string != "--start"):
+        print("Error: fourth argument should be --start\n")
+    if(goal_string != "--goal"):
+        print("Error: sixth argument should be --goal\n")
+    if(search_string != "--search"):
+        print("Error: eighth argument should be --search\n")
 
-if(input_string != "--input"):
-    print("Error: second argument should be --input\n")
-if(start_string != "--start"):
-    print("Error: fourth argument should be --start\n")
-if(goal_string != "--goal"):
-    print("Error: sixth argument should be --goal\n")
-if(search_string != "--search"):
-    print("Error: eighth argument should be --search\n")
+#print(start_node[0])
 
 '''
 #print("Total arguments passed:", n)
